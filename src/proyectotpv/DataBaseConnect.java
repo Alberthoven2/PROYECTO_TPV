cfa751c8d771cd423a9b5db8692bc0f9562163f1
Alberthoven2 <alb.1980@hotmail.com>
2018-06-29 03:41:16 +0200

    Añado la clase para conectar con la base de datos

---
+++ src/proyectotpv/DataBaseConnect.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectotpv;
+
+import java.sql.*;
+
+/**
+ *
+ * @author alb19
+ */
+public class DataBaseConnect {
+    
+    String ruta;
+    String usuario;
+    String password;
+    Connection con;
+    
+    public DataBaseConnect(){
+        
+        this.ruta = "jdbc:mysql://localhost:3306/myjavaapp1";
+        this.usuario = "root";
+        this.password = "";
+        try{
+            con = DriverManager.getConnection(ruta, usuario,password);
+        }catch(SQLException sqle){
+            sqle.getStackTrace();
+        }
+    }
+    
+}
